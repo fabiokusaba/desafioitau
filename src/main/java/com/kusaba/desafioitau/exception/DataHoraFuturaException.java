de7b3d5cb40869d00d34ad99292f9f8cbcd526b3
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-26 21:08:16 -0300

    feat: criação da exceção de data e hora futura

---
+++ src/main/java/com/kusaba/desafioitau/exception/DataHoraFuturaException.java
@@ -0,0 +1,7 @@
+package com.kusaba.desafioitau.exception;
+
+public class DataHoraFuturaException extends RuntimeException {
+    public DataHoraFuturaException(String message) {
+        super(message);
+    }
+}
